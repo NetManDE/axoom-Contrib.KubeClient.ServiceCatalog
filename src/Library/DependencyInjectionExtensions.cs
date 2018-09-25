20c03aa2cfacce7c7448cd0d0c8ce3738084a5e9
Bastian Eicher <bastian.eicher@axoom.com>
2018-09-25 13:03:06 +0200

    Redesigns DI extension methods

--- src/Library/DependencyInjectionExtensions.cs
+++ src/Library/DependencyInjectionExtensions.cs
@@ -2,7 +2,6 @@ using System;
 using System.Diagnostics.CodeAnalysis;
 using Contrib.KubeClient.CustomResources;
 using JetBrains.Annotations;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Kubernetes.ServiceCatalog.Models
@@ -11,47 +10,49 @@ namespace Kubernetes.ServiceCatalog.Models
     [PublicAPI]
     public static class DependencyInjectionExtensions
     {
-        public static IServiceCollection AddClusterServiceClassCustomResource(this IServiceCollection services)
-            => services.AddCustomResourceWatcher<ClusterServiceClass, ClusterServiceClassStore>("stable.axoom.com", "clusterServiceClass")
-                       .AddSingleton<IClusterServiceClassStore>(prov => prov.GetRequiredService<ClusterServiceClassStore>());
+        private const string AxoomApiVersion = "stable.axoom.com";
 
-        public static IServiceProvider UseClusterServiceClassCustomResource(this IApplicationBuilder app)
-            => UseCustomResource<ClusterServiceClassStore>(app);
+        public static IServiceCollection AddClusterServiceClassStore(this IServiceCollection services)
+        {
+            return services.AddCustomResourceWatcher<ClusterServiceClass, ClusterServiceClassStore>(AxoomApiVersion, "clusterServiceClass")
+                           .AddSingleton<IClusterServiceClassStore>(prov => prov.GetRequiredService<ClusterServiceClassStore>());
+        }
 
-        public static IServiceCollection AddServiceInstanceCustomResource(this IServiceCollection services)
-            => services.AddCustomResourceWatcher<ServiceInstance, ServiceInstanceStore>("stable.axoom.com", "serviceInstance")
+        public static IServiceProvider UseClusterServiceClassStore(this IServiceProvider provider)
+            => UseStore<ClusterServiceClassStore>(provider);
+
+        public static IServiceCollection AddServiceInstanceStore(this IServiceCollection services)
+            => services.AddCustomResourceWatcher<ServiceInstance, ServiceInstanceStore>(AxoomApiVersion, "serviceInstance")
                        .AddSingleton<IServiceInstanceStore>(prov => prov.GetRequiredService<ServiceInstanceStore>());
 
-        public static IServiceProvider UseServiceInstanceCustomResource(this IApplicationBuilder app)
-            => UseCustomResource<ServiceInstanceStore>(app);
+        public static IServiceProvider UseServiceInstanceStore(this IServiceProvider provider)
+            => UseStore<ServiceInstanceStore>(provider);
 
-        public static IServiceCollection AddClusterServicePlanCustomResource(this IServiceCollection services)
-            => services.AddCustomResourceWatcher<ClusterServicePlan, ClusterServicePlanStore>("stable.axoom.com", "clusterServicePlan")
+        public static IServiceCollection AddClusterServicePlanStore(this IServiceCollection services)
+            => services.AddCustomResourceWatcher<ClusterServicePlan, ClusterServicePlanStore>(AxoomApiVersion, "clusterServicePlan")
                        .AddSingleton<IClusterServicePlanStore>(prov => prov.GetRequiredService<ClusterServicePlanStore>());
 
-        public static IServiceProvider UseClusterServicePlanCustomResource(this IApplicationBuilder app)
-            => UseCustomResource<ClusterServicePlanStore>(app);
+        public static IServiceProvider UseClusterServicePlanStore(this IServiceProvider provider)
+            => UseStore<ClusterServicePlanStore>(provider);
 
-        public static IServiceCollection AddClusterServiceBrokerCustomResource(this IServiceCollection services)
-            => services.AddCustomResourceWatcher<ClusterServiceBroker, ClusterServiceBrokerStore>("stable.axoom.com", "clusterServiceBroker")
+        public static IServiceCollection AddClusterServiceBrokerStore(this IServiceCollection services)
+            => services.AddCustomResourceWatcher<ClusterServiceBroker, ClusterServiceBrokerStore>(AxoomApiVersion, "clusterServiceBroker")
                        .AddSingleton<IClusterServiceBrokerStore>(prov => prov.GetRequiredService<ClusterServiceBrokerStore>());
 
-        public static IServiceProvider UseClusterServiceBrokerCustomResource(this IApplicationBuilder app)
-            => UseCustomResource<ClusterServiceBrokerStore>(app);
+        public static IServiceProvider UseClusterServiceBrokerStore(this IServiceProvider provider)
+            => UseStore<ClusterServiceBrokerStore>(provider);
 
-        public static IServiceCollection AddServiceBindingCustomResource(this IServiceCollection services)
-            => services.AddCustomResourceWatcher<ServiceBinding, ServiceBindingStore>("stable.axoom.com", "serviceBinding")
+        public static IServiceCollection AddServiceBindingStore(this IServiceCollection services)
+            => services.AddCustomResourceWatcher<ServiceBinding, ServiceBindingStore>(AxoomApiVersion, "serviceBinding")
                        .AddSingleton<IServiceBindingStore>(prov => prov.GetRequiredService<ServiceBindingStore>());
 
-        public static IServiceProvider UseServiceBindingCustomResource(this IApplicationBuilder app)
-            => UseCustomResource<ServiceBindingStore>(app);
+        public static IServiceProvider UseServiceBindingStore(this IServiceProvider provider)
+            => UseStore<ServiceBindingStore>(provider);
 
-        public static IServiceProvider UseCustomResource<TCustomResourceStore>(this IApplicationBuilder app)
+        public static IServiceProvider UseStore<TCustomResourceStore>(this IServiceProvider provider)
             where TCustomResourceStore : ICustomResourceWatcher
         {
-            var provider = app.ApplicationServices;
-            var store = provider.GetRequiredService<TCustomResourceStore>();
-            store.StartWatching();
+            provider.GetRequiredService<TCustomResourceStore>().StartWatching();
             return provider;
         }
     }
